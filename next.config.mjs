e7c7981139b6b61732509ac29d97a9da5a45d912
Alisher Uralov <aliwerdev@gmail.com>
2024-07-30 21:25:01 +0500

    fsadf

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   env: {
     HOST_API: "http://localhost:2020/api", //localhost
     // HOST_API: "http://95.130.227.142:2020/api", //husanboy
-    // HOST_API: "https://api-cards.scripter.uz/api", //real server
+    HOST_API: "https://api-cards.scripter.uz/api", //real server
     SETTINGS_STORAGE_KEY: "APP_SETTINGS",
     TOKEN_STORAGE_KEY: "client_token",
     GOOGLE_CLIENT_ID: "990218029399-hm5qqkijnueabi48ukp39dqitm6980ds.apps.googleusercontent.com",
