ca0e981ecf6c3599c1bf1addb659bb829ddf22b1
Alisher Uralov <aliwerdev@gmail.com>
2024-07-30 12:48:30 +0500

    fsdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 const nextConfig = {
   env: {
     // HOST_API: "http://localhost:2020/api",
-    HOST_API: "http://95.130.227.142:2020/api",
+    // HOST_API: "http://95.130.227.142:2020/api",
+    HOST_API: "https://flashcards-org.vercel.app/api",
     SETTINGS_STORAGE_KEY: "APP_SETTINGS",
     TOKEN_STORAGE_KEY: "client_token",
     GOOGLE_CLIENT_ID: "990218029399-hm5qqkijnueabi48ukp39dqitm6980ds.apps.googleusercontent.com",
